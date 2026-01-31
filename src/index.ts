4e8d98f7842fb9bfda50ffa0d25c0ad92868211c
kkavyaba <kkavyaba@blueplanet.com>
2026-01-31 15:31:36 +0530

    index.ts to run and  test the code implementation

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { UserRepository } from "./repositories/user.repository.js";
+import { EmailService } from "./services/email.service.js";
+
+
+//initialize repository and services (create instances)
+const userRepository = new UserRepository();
+const emailService = new EmailService();
+
+console.log("User Repository and Email Service initialized.");
+
+
+//create a new user
+const newUserResult = userRepository.create({
+name: "Kavya Barodia",
+email: "kavya.barodia@example.com",
+age: 22
+});
+
+
+
+if(newUserResult.success) {
+    console.log("User created successfully:", newUserResult.data);
+ }
+else {
+    console.error("Error creating user:", newUserResult.error);
+}
